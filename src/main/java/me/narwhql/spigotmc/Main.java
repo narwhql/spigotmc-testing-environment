27a7e29b755772ccfc9a83f0e98e3cb5232dc924
narwhql <roniemam2005@gmail.com>
2022-04-15 17:21:31 +0200

    New registrations

--- src/main/java/me/narwhql/spigotmc/Main.java
+++ src/main/java/me/narwhql/spigotmc/Main.java
@@ -1,22 +1,32 @@
 package me.narwhql.spigotmc;
 
 import me.narwhql.spigotmc.commands.ArmorStandCommand;
+import me.narwhql.spigotmc.commands.ChatColorCommand;
 import me.narwhql.spigotmc.commands.PetCommand;
+import me.narwhql.spigotmc.listeners.AsyncPlayerChatListener;
 import me.narwhql.spigotmc.pets.PetManager;
+import me.narwhql.spigotmc.player.PlayerManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     private static Main instance;
 
+    private MySQL mySQL;
+
+    private PlayerManager playerManager;
     private PetManager petManager;
 
     @Override
     public void onEnable() {
         instance = this;
 
+        this.mySQL = new MySQL();
+        this.mySQL.connect();
+
         this.initCommands();
         this.initListeners();
 
+        this.playerManager = new PlayerManager(this);
         this.petManager = new PetManager(this);
     }
 
@@ -33,14 +43,21 @@ public class Main extends JavaPlugin {
         return petManager;
     }
 
+    public PlayerManager getPlayerManager() {
+        return playerManager;
+    }
+
+    public MySQL getMySQL() {
+        return mySQL;
+    }
+
     private void initCommands() {
         this.getCommand("armorstand").setExecutor(new ArmorStandCommand());
-        this.getCommand("send").setExecutor(new SendCommand());
         this.getCommand("pet").setExecutor(new PetCommand());
-        this.getCommand("block").setExecutor(new BlockCommand());
+        this.getCommand("chatcolor").setExecutor(new ChatColorCommand());
     }
 
     private void initListeners() {
-
+        this.getServer().getPluginManager().registerEvents(new AsyncPlayerChatListener(), this);
     }
 }
